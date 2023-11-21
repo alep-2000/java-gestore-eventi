8d76cf2fa13e7605ddc0b5b91a00346d009bd63e
alep-2000 <alessiopiras056@gmail.com>
2023-11-21 10:05:13 +0100

    Creazione classe Evento + controllo errori data e posti totali

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,18 @@
 package org.lessons.java;
 
-public class Main {
+import java.time.LocalDate;
 
+
+import org.lessons.java.pojo.Evento;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		try {
+			Evento e = new Evento("Festa", LocalDate.parse("2023-11-21"), 30, 10);
+            System.out.println(e);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+	}
 }
--- src/org/lessons/java/pojo/Evento.java
+++ src/org/lessons/java/pojo/Evento.java
@@ -1,5 +1,68 @@
 package org.lessons.java.pojo;
 
+import java.time.LocalDate;
+
 public class Evento {
+	private String titolo;
+	private LocalDate data;
+	private int nPostiTotali;
+	private int nPrenotati;
 	
+	public Evento(String titolo, LocalDate data, int nPostiTotali, int nPrenotati){
+			setTitolo(titolo);
+			setData(data);
+			setnPostiTotali(nPostiTotali);
+			setnPrenotati(0);
+	}
+	
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+		
+		 
+        if (this.data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento è già passata.");
+        }
+	}
+
+	public int getnPostiTotali() {
+		return nPostiTotali;
+	}
+
+	private void setnPostiTotali(int nPostiTotali) {
+		
+        if (nPostiTotali <= 0) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo.");
+        }
+        
+		this.nPostiTotali = nPostiTotali;
+	}
+
+	public int getnPrenotati() {
+		return nPrenotati;
+	}
+
+	private void setnPrenotati(int nPrenotati) {
+		this.nPrenotati = nPrenotati;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Titolo: " + getTitolo() + "\n"
+				+ "Data: " + getData()	+ "\n"
+				+ "Numero Posti Totali: " + getnPostiTotali() + "\n"
+				+ "Numero Posti Prenotati: " + getnPrenotati();
+	}
 }
